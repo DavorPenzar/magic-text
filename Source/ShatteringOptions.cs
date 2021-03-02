a569b1d058b1cf2cf18887db6dc9c232c34301b2
Davor Penzar <davor.penzar@gmail.com>
2021-03-02 15:16:19 +0100

    Add files via upload

--- Source/ShatteringOptions.cs
+++ Source/ShatteringOptions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.IO;
 
 namespace MagicText
@@ -16,6 +17,7 @@ namespace MagicText
         ///         Actual implementations of <see cref="ITokeniser" /> interface may define what exactly an <em>empty</em> token means, but usually this would be a <c>null</c> or a string yielding <c>true</c> when checked via <see cref="String.IsNullOrEmpty(String)" /> or <see cref="String.IsNullOrWhiteSpace(String)" /> method.
         ///     </para>
         /// </remarks>
+        [DefaultValue(false)]
         public Boolean IgnoreEmptyTokens { get; set; } = false;
 
         /// <value>Indicator if line ends should be ignored. If <c>false</c>, they should be represented by <see cref="LineEndToken" />s. Default is <c>false</c>.</value>
@@ -24,6 +26,7 @@ namespace MagicText
         ///         Line ends should be considered both the new line character (CR, LF and CRLF) and the end of the input.
         ///     </para>
         /// </remarks>
+        [DefaultValue(false)]
         public Boolean IgnoreLineEnds { get; set; } = false;
 
         /// <value>Inidcator if empty lines should be ignored, i. e. not produce any tokens. If <c>true</c>, they should not produce even <see cref="LineEndToken" />; if <c>false</c>, they should be represented by <see cref="EmptyLineToken" />s. Default is <c>false</c>.</value>
@@ -32,6 +35,7 @@ namespace MagicText
         ///         Empty lines should be considered those lines that produce no tokens. This should be checked <strong>after</strong> filtering empty tokens out from the line if <see cref="IgnoreEmptyTokens" /> is <c>true</c>.
         ///     </para>
         /// </remarks>
+        [DefaultValue(false)]
         public Boolean IgnoreEmptyLines { get; set; } = false;
 
         /// <value>Token to represent the line end. Default is <see cref="Environment.NewLine" />.</value>
@@ -44,6 +48,7 @@ namespace MagicText
         ///         If a line is discarded as empty (if <see cref="IgnoreEmptyLines" /> is <c>true</c>), it should not produce a <see cref="LineEndToken" />.
         ///     </para>
         /// </remarks>
+        [DefaultValue("\n")] // <-- this may be different from `Environment.NewLine`
         public String? LineEndToken { get; set; } = Environment.NewLine;
 
         /// <value>Token to represent an empty line. Default is <see cref="String.Empty" />.</value>
@@ -52,6 +57,7 @@ namespace MagicText
         ///         If a line does not produce any token (after ignoring empty tokens if <see cref="IgnoreEmptyTokens" /> is <c>true</c>) but should not be discarded (if <see cref="IgnoreEmptyLines" /> is <c>false</c>), it should be represented by <see cref="EmptyLineToken" />. This should be done even if <see cref="EmptyLineToken" /> would be considered an empty token and empty tokens should be ignored (if <see cref="IgnoreEmptyTokens" /> is <c>true</c>).
         ///     </para>
         /// </remarks>
+        [DefaultValue("")]
         public String? EmptyLineToken { get; set; } = String.Empty;
     }
 }
