89a1be4bd3ebb706e0fb3544d3f0cefedfdea6c2
Davor Penzar <davor.penzar@gmail.com>
2022-09-27 19:20:00 +0200

    Initialised a benchmark testing project

---
+++ MagicText.BenchmarkTesting/README.md
@@ -0,0 +1,3 @@
+#   MagicText &ndash; Benchmark Testing
+
+Benchmark tests for the [`MagicText`](http://github.com/DavorPenzar/magic-text/) library.
---
+++ MagicText.BenchmarkTesting/src/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MagicText.Example
+{
+    public static class Program
+    {
+        public const int ExitSuccess = 0;
+        public const int ExitFailure = -1;
+
+        public static Int32 Main(String[]? args = null)
+        {
+            return ExitSuccess;
+        }
+    }
+}
